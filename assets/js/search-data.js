8eb8aeb623586e6cc8ccb08a123ad037e01a6342
shashankpratapsingh <66408186+shashankpratapsingh@users.noreply.github.com>
2025-11-13 23:20:56 +0000

    Deploying to gh-pages from @ shashankpratapsingh/shashankpratapsingh.github.io@50be1750fbc479ab4a5645e2df83bc2d6ad4e8cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Refereed publications, preprints, and notes.",
+          description: "No refereed publications yet.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
