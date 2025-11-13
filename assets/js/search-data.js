9c6b9133f6334164f30395f09a34c3b1a18d3c2c
shashankpratapsingh <66408186+shashankpratapsingh@users.noreply.github.com>
2025-11-13 10:49:38 +0000

    Deploying to gh-pages from @ shashankpratapsingh/shashankpratapsingh.github.io@e4d29ffcbec2a0349439d27e15e21edd8e539b8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,19 +4,12 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-photography",
-          title: "Photography",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/photography/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
